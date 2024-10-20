58eaff32aee380229891254089e32aba2e6bbda4
barilo <barilo>
2024-10-20 21:28:01 +0300

    Initial commit

---
+++ src/main/java/tgb/cryptoexchange/cryptowithdrawal/CryptoWithdrawalApplication.java
@@ -0,0 +1,13 @@
+package tgb.cryptoexchange.cryptowithdrawal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoWithdrawalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoWithdrawalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tgb/cryptoexchange/cryptowithdrawal/CryptoWithdrawalApplicationTests.java
@@ -0,0 +1,13 @@
+package tgb.cryptoexchange.cryptowithdrawal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoWithdrawalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
