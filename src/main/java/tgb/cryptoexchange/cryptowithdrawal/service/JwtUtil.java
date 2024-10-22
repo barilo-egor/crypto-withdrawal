2669132ee565506d3ee9f1d831baf5322fcd9963
barilo <e.barilo@isida.by>
2024-10-22 15:13:14 +0300

    асимметричный ключ + config.yml

--- src/main/java/tgb/cryptoexchange/cryptowithdrawal/service/JwtUtil.java
+++ src/main/java/tgb/cryptoexchange/cryptowithdrawal/service/JwtUtil.java
@@ -3,14 +3,17 @@ package tgb.cryptoexchange.cryptowithdrawal.service;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.io.Decoders;
-import io.jsonwebtoken.security.Keys;
+import org.bouncycastle.util.io.pem.PemReader;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
-import javax.crypto.SecretKey;
-import java.security.Key;
-import java.util.Base64;
+import java.io.FileReader;
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -18,13 +21,38 @@ import java.util.function.Function;
 
 @Component
 public class JwtUtil {
+    private final PrivateKey privateKey;
 
-    @Value("${jwt.key}")
-    private String secretKey;
+    private final PublicKey publicKey;
 
-    private Key getSigningKey() {
-        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
-        return Keys.hmacShaKeyFor(keyBytes);
+    private final long tokenExpirationMs;
+
+    public JwtUtil(
+            @Value("${jwt.privateKeyPath}") Resource privateKeyPath,
+            @Value("${jwt.publicKeyPath}") Resource publicKeyPath,
+            @Value("${jwt.tokenExpirationMs}") long tokenExpirationMs) throws Exception {
+
+        this.privateKey = loadPrivateKey(privateKeyPath);
+        this.publicKey = loadPublicKey(publicKeyPath);
+        this.tokenExpirationMs = tokenExpirationMs;
+    }
+
+    private PrivateKey loadPrivateKey(Resource resource) throws Exception {
+        PemReader pemReader = new PemReader(new FileReader(resource.getFile()));
+        byte[] keyBytes = pemReader.readPemObject().getContent();
+        pemReader.close();
+        PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(keyBytes);
+        KeyFactory kf = KeyFactory.getInstance("RSA");
+        return kf.generatePrivate(spec);
+    }
+
+    private PublicKey loadPublicKey(Resource resource) throws Exception {
+        PemReader pemReader = new PemReader(new FileReader(resource.getFile()));
+        byte[] keyBytes = pemReader.readPemObject().getContent();
+        pemReader.close();
+        X509EncodedKeySpec spec = new X509EncodedKeySpec(keyBytes);
+        KeyFactory kf = KeyFactory.getInstance("RSA");
+        return kf.generatePublic(spec);
     }
 
     public String generateToken(String username) {
@@ -33,8 +61,8 @@ public class JwtUtil {
                 .setClaims(claims)
                 .setSubject(username)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 3600000)) // 1 час
-                .signWith(getSigningKey())
+                .setExpiration(new Date(System.currentTimeMillis() + tokenExpirationMs)) // 1 час
+                .signWith(privateKey, SignatureAlgorithm.RS256)
                 .compact();
     }
 
@@ -54,7 +82,7 @@ public class JwtUtil {
 
     private Claims extractAllClaims(String token) {
         return Jwts.parserBuilder()
-                .setSigningKey(getSigningKey())
+                .setSigningKey(publicKey)
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
