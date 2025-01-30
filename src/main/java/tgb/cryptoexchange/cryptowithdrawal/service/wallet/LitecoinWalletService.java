4b04f1991adae85b4eeef6a7ad552f61bdeabd8a
barilo <barilo>
2025-01-30 22:53:10 +0300

    fix currency

--- src/main/java/tgb/cryptoexchange/cryptowithdrawal/service/wallet/LitecoinWalletService.java
+++ src/main/java/tgb/cryptoexchange/cryptowithdrawal/service/wallet/LitecoinWalletService.java
@@ -3,6 +3,7 @@ package tgb.cryptoexchange.cryptowithdrawal.service.wallet;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import tgb.cryptoexchange.enums.CryptoCurrency;
 
 @Service
 public class LitecoinWalletService extends ElectrumWalletService {
@@ -35,4 +36,8 @@ public class LitecoinWalletService extends ElectrumWalletService {
         return rpcPassword;
     }
 
+    @Override
+    public CryptoCurrency getCryptoCurrency() {
+        return CryptoCurrency.LITECOIN;
+    }
 }
