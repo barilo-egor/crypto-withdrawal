5ef85b74aba87d6e0652a418e5e41d1138879b91
barilo <barilo>
2025-02-16 12:52:26 +0300

    try fix

--- src/main/java/tgb/cryptoexchange/cryptowithdrawal/vo/RestoreWalletElectrumRequest.java
+++ src/main/java/tgb/cryptoexchange/cryptowithdrawal/vo/RestoreWalletElectrumRequest.java
@@ -1,14 +1,17 @@
 package tgb.cryptoexchange.cryptowithdrawal.vo;
 
-import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.SuperBuilder;
+
+import java.util.List;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
 @SuperBuilder
 public class RestoreWalletElectrumRequest extends ElectrumRequest {
+
+    private List<Object> params;
 
     private final String method = "restore";
 }
